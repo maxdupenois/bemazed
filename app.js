8c61ef47680784ad2e94beac48d14b99c43b10df
Max <max.dupenois@forward.co.uk>
2014-03-15 12:45:00 +0000

    Initial app code

---
+++ app.js
@@ -0,0 +1,32 @@
+var default_port = 5000;
+var express = require('express')
+  , url = require('url')
+  , fs = require("fs")
+  , app = express()
+  , server = require('http').createServer(app).listen(default_port)
+  , io = require('engine.io').attach(server)
+  , logfmt = require('logfmt')
+  ;
+
+
+app.configure(function(){
+});
+
+app.use(logfmt.requestLogger());
+
+app.use(function(req, res, next){  
+  // DEBUG CODE GOES HERE
+  // console.log('%s %s', req.method, req.url);
+  next();
+});
+
+
+app.get('/', function(req, res, next){
+  res.send('hello there');
+});
+
+
+server.listen(process.env.PORT || default_port, function(){
+  console.log('\033[90mlistening on localhost:'+default_port+' \033[39m');
+});
+
